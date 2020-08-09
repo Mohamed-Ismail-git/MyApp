c733864ca5529a115f51c3878ad4fbf6df2cdd0a
ismail <ismailkatania16@gmail.com>
2020-08-09 15:45:09 +0200

    just four test register user

---
+++ app/src/main/java/dz/ochefaouiismail/mylogin/Constants.java
@@ -0,0 +1,9 @@
+package dz.ochefaouiismail.mylogin;
+
+public class Constants {
+
+    private static final String ROOT_URL ="http://192.168.8.101:8080/test/";
+   public static final String URL_Register =ROOT_URL+"user_control.php";
+
+
+}
--- app/src/main/java/dz/ochefaouiismail/mylogin/MainActivity.java
+++ app/src/main/java/dz/ochefaouiismail/mylogin/MainActivity.java
@@ -2,19 +2,45 @@ package dz.ochefaouiismail.mylogin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     TextView Signup;
     TextView LoginBtn;
+    EditText email,password;
+    private ProgressDialog progressDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //variable
+        email=findViewById(R.id.ETEmail);
+        password=findViewById(R.id.ETPassword);
+        progressDialog = new ProgressDialog(this);
         //init textviews
         init();
     }
@@ -35,11 +61,56 @@ public class MainActivity extends AppCompatActivity {
         LoginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                RegisterUser();
+
                 startActivity(new Intent(MainActivity.this,PrincScreen.class));
                 finish();
 
             }
         });
+
+
+    }
+    private void RegisterUser(){
+        final String Email = email.getText().toString().trim();
+        final String Password = password.getText().toString().trim();
+        progressDialog.setMessage("registering User...");
+        progressDialog.show();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, Constants.URL_Register, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                progressDialog.dismiss();
+                JSONObject jsonObject;
+                try {
+                    jsonObject = new JSONObject(response);
+                    Toast.makeText(getApplicationContext(),jsonObject.getString("success"),Toast.LENGTH_LONG).show();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                progressDialog.hide();
+                Toast.makeText(getApplicationContext(),error.getMessage(),Toast.LENGTH_LONG).show();
+
+            }
+        }){
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> params =new HashMap<>();
+                params.put("email", Email);
+                params.put("password", Password);
+                return params;
+            }
+        };
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+
 
 
     }
